3ad8e1f8ae1effb3286a0aeafa1df94ba7a9f9a3
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2022-11-29 13:27:05 +0530

    2nd commit from Adheesha

---
+++ contact-us.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/main.css">
+	<link rel="stylesheet" href="./css/contact-us.css">
+    <title>Contact us</title>
+</head>
+<body>
+    <div class="navbar">
+        <h2>TapToBus</h2>
+
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./terms-conditions.php">Terms & Conditions</a></li>
+            <li><a href="./contact-us.php">Contact Us</a></li>
+        </ul>
+    </div>
+
+    <div class="content">
+        <h1>Contact Us</h1>
+
+        <form name="contact-us-form" action="" method="" novalidate>
+            <div class="name">
+                <label for="name">Name</label> <br>
+                <input type="text" name="name" placeholder="Enter your name" maxlength="50">
+            </div>
+
+            <div class="email">
+                <label for="email">Email</label> <br>
+                <input type="email" name="email" placeholder="Enter your email" maxlength="100">
+            </div>
+
+            <div class="mobile">
+                <label for="mobile">Mobile No</label> <br>
+                <input type="tel" name="mobile" placeholder="Enter your mobile no" maxlength="10">
+            </div>
+
+            <div class="sub">
+                <label for="sub">Subject</label> <br>
+                <select name="sub">
+                    <option value="" disabled selected>Select subject</option>
+                    <option value="info">Getting information</option>
+                    <option value="tech">Technical issue</option>
+                    <option value="pay">Payment issue</option>
+                    <option value="serv">Service issue</option>
+                    <option value="other">Other</option>
+                </select>
+            </div>
+
+            <div class="msg">
+                <label for="msg">Message</label> <br>
+                <textarea name="msg" rows="4" placeholder="Enter your message..." maxlength="200"></textarea>
+            </div>
+
+            <div class="btn">
+                <button><span></span>Send</button>
+            </div>
+        </form>
+    </div>
+
+    <div class="footer">
+        <p>© 2022, All rights reserved by TapToBus Cooperation Pvt. Ltd.<br>
+            No: 1001, Reid Avenue, Colombo 7, Sri Lanka.</p>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./css/main.css">
+	<link rel="stylesheet" href="./css/index.css">
+    <title>TapToBus</title>
+</head>
+<body>
+    <div class="navbar">
+        <h2>TapToBus</h2>
+
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href="./terms-conditions.php">Terms & Conditions</a></li>
+            <li><a href="./contact-us.php">Contact Us</a></li>
+        </ul>
+    </div>
+
+    <div class="content">
+        <h1>BOOK YOUR SEATS</h1>
+
+        <p>You can easily book seats online on expressway buses via TapToBus</p>
+
+		<div class="btn">
+			<a href="./login.php"><button><span></span>Log In</button></a>
+			<a href="./signup-type.php"><button><span></span>Sign Up</button></a>
+		</div>
+    </div>
+</body>
+</html>
